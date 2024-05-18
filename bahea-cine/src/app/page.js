6256d236b525c9126ed65235d8dea1113d901423
Caio Hisamatsu <caiomizohata@gmail.com>
2024-05-18 21:07:21 +0000

    Revert "menu nav"
    This reverts commit 59729c1a624fb610b0263eac77c4efc238c46737.

--- bahea-cine/src/app/page.js
+++ bahea-cine/src/app/page.js
@@ -4,20 +4,11 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header>
-            <div className="container">
-                <div className="logo"><img src="img/logo.png"></img></div>
-                <div className="menu">
-                    <nav>
-                        <a href="#">Bomboniere</a>
-                        <a href="#">Cartaz</a>
-                        <a href="#">Lançamentos</a>
-                        <a href="#">Contato</a>
-                        <a href="#">Sobre</a>
-                    </nav>
-                </div>
-            </div>
-        </header>
+      <div className="Menu">
+        <a>Cartaz de Lançamentos</a>
+        <a>Lançamentos</a>
+      </div>
+      
     </main>
   );
 }
