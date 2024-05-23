e0941740b25c04456049ad953955e1d46324ce4a
Caio Hisamatsu <caiomizohata@gmail.com>
2024-05-23 23:12:39 +0000

    fixed tag '<a>'

--- bahea-cine/src/app/login/page.jsx
+++ bahea-cine/src/app/login/page.jsx
@@ -4,7 +4,8 @@ export default function Login() {
     return (
         <main>
             <h1>Tela de login</h1>
-            <span>Cadastrar-se</span> <a href="/cadastro">Cadastrar-se</a>
+            <span>Cadastrar-se</span> 
+            <a href="/cadastro">Cadastrar-se</a>
         </main>
     )
 }
