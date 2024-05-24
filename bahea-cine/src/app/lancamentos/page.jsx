2834611ee6b50f080ea14d0f212858530faccf25
Otávio Pires C.A. Falcão <otavioprogramador017@gmail.com>
2024-05-24 17:17:14 +0000

    Add lancamentos

--- bahea-cine/src/app/lancamentos/page.jsx
+++ bahea-cine/src/app/lancamentos/page.jsx
@@ -1,9 +1,31 @@
-import styles from "./page.style.css";
+import React from 'react';
+import './page.style.css';
 
-export default function Lancamentos() {
-    return (
-        <main>
-            <h1>Pagina de Lançamentos</h1>
-        </main>
-    )
+const Page = () => {
+  return (
+    <div className="container">
+      <h1>Estreias</h1>
+      <h2>Filmes</h2>
+      <div className="movies">
+        <div className="movie-card">
+          <div className="poster"></div>
+          <p>Estreia</p>
+        </div>
+        <div className="movie-card">
+          <div className="poster"></div>
+          <p>Estreia</p>
+        </div>
+        <div className="movie-card">
+          <div className="poster"></div>
+          <p>Estreia</p>
+        </div>
+        <div className="movie-card">
+          <div className="poster"></div>
+          <p>Estreia</p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Page;
