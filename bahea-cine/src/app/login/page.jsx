135b04f9b6d33278f6ff80a0f564fb81d097449b
antonio duarte <antoniodavi220404@gmail.com>
2024-05-25 01:20:58 +0000

    Tela login e cadaastro

--- bahea-cine/src/app/login/page.jsx
+++ bahea-cine/src/app/login/page.jsx
@@ -1,11 +1,29 @@
-import styles from "./page.style.css";
+import React from 'react';
+import './page.style.css';
 
-export default function Login() {
-    return (
-        <main>
-            <h1>Tela de login</h1>
-            <span>Cadastrar-se</span> 
-            <a href="/cadastro">Cadastrar-se</a>
-        </main>
-    )
-}
+const Page = () => {
+  return (
+    <div className="login-container">
+      <a href="/">
+        <img src="img/logo.png" alt="Bahea Cine Logo" className="logo" />
+      </a>
+      <div className="login-title-container">
+        <img src="img/login.png" alt="Logo" className="title-logo" />
+        <h2 className="login-title">Login</h2>
+        <img src="img/login.png" alt="Logo" className="title-logo" />
+      </div>
+      <form className="login-form">
+        <input type="text" placeholder="Usuário" className="login-input" />
+        <input type="password" placeholder="Senha" className="login-input" />
+        <button type="submit" className="login-button">Entrar</button>
+      </form>
+      <div className="options">
+        <a href="/cadastro" className="option-link">Fazer Cadastro</a>
+        <span className="option-divider">|</span>
+        <a href="/" className="option-link">Página Inicial</a>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
