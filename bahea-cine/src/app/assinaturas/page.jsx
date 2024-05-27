6901ed4c7607ab5b27ff63418294fccee01e1f77
Caio Hisamatsu <134729130+caioeduardo4100@users.noreply.github.com>
2024-05-27 15:52:21 -0300

    Tela de assinatura e compras

---
+++ bahea-cine/src/app/assinaturas/page.jsx
@@ -0,0 +1,7 @@
+import './page.style.css';
+
+export default function Assinaturas() {
+    return (
+        <h1>Tela das assinaturas</h1>
+    )
+}
---
+++ bahea-cine/src/app/compras/page.jsx
@@ -0,0 +1,7 @@
+import './page.style.css';
+
+export default function Compras() {
+    return (
+        <h1>Tela das compras</h1>
+    )
+}
