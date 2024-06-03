97cb6247a816cd00d01cc171f1f537bd150ebf43
Guilherme Pedro <guibarros080890@gmail.com>
2024-06-03 19:36:49 +0000

    tela compras, lançamentos, pré-vendas GUILHERME

--- bahea-cine/src/app/compras/page.jsx
+++ bahea-cine/src/app/compras/page.jsx
@@ -1,45 +1,46 @@
 import "./page.style.css";
 
 export default function Compras() {
-    return (
-        <div className="container">
-            <h1 className="title">Compras</h1>
-            <div className="content">
-                <div className="left-content">
-                    <h2 className="subtitle">Produtos Selecionados</h2>
-                    <table className="product-table">
-                        <thead>
-                            <tr>
-                                <th>Produto</th>
-                                <th>Quantidade</th>
-                                <th>Preço Unitário</th>
-                                <th>Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Produto 1</td>
-                                <td>1</td>
-                                <td>R$ 10,00</td>
-                                <td>R$ 10,00</td>
-                            </tr>
-                            <tr>
-                                <td>Produto 2</td>
-                                <td>2</td>
-                                <td>R$ 15,00</td>
-                                <td>R$ 30,00</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className="right-content">
-                    <div className="total">
-                        <h2 className="subtitle">Total a Pagar</h2>
-                        <p className="amount">R$ 40,00</p>
-                        <p className="payment-message">O pagamento é presencial</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <h1 className="title">Compras</h1>
+      <div className="content">
+        <div className="left-content">
+          <h2 className="subtitle">Produtos Selecionados</h2>
+          <table className="product-table">
+            <thead>
+              <tr>
+                <th>Produto</th>
+                <th>Quantidade</th>
+                <th>Preço Unitário</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Produto 1</td>
+                <td>1</td>
+                <td>R$ 10,00</td>
+                <td>R$ 10,00</td>
+              </tr>
+              <tr>
+                <td>Produto 2</td>
+                <td>2</td>
+                <td>R$ 15,00</td>
+                <td>R$ 30,00</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-    );
+        <div className="right-content">
+          <div className="total">
+            <h2 className="subtitle">Total a Pagar</h2>
+            <p className="amount">R$ 40,00</p>
+            <p className="payment-message">O pagamento é presencial</p>
+          </div>
+        </div>
+      </div>
+      <a href={"/"} className="back-link">Voltar para a Página Inicial</a>
+    </div>
+  );
 }
--- bahea-cine/src/app/contato/page.jsx
+++ bahea-cine/src/app/contato/page.jsx
@@ -10,21 +10,9 @@ export default function Contato() {
           <div className="contact-header">
             <h1>Contato</h1>
           </div>
-          <form>
-            <div className="input-group">
-              <label htmlFor="name">Nome</label>
-              <input type="text" id="name" name="name" placeholder="Nome" required />
-            </div>
-            <div className="input-group">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" placeholder="Email" required />
-            </div>
-            <div className="input-group">
-              <label htmlFor="message">Mensagem</label>
-              <textarea id="message" name="message" placeholder="Mensagem" required></textarea>
-            </div>
-            <button type="submit">Enviar Mensagem</button>
-          </form>
+          <div className="contact-info">
+            <p>Para entrar em contato conosco mande um email para: baheacine@gmail.com ou ligue para (14)99999-9999</p>
+          </div>
         </div>
         <button className="back-button" onClick={() => window.history.back()}>Voltar</button>
       </div>
